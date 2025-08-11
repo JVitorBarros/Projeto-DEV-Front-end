c63ab8e67af02ae8fd534e93193decd581626cf2
João Vitor Da Silva Barros <jvitorbarros84@gmail.com>
2025-08-11 15:38:04 -0300

    add conteudo

---
+++ 2-JAVASCRIPT/7-funcoes/3-arrow-function/aula01.js
@@ -0,0 +1,6 @@
+const soma = (a, b) =>{
+    let resultado = a + b;
+    return resultado;
+};
+
+console.log(soma(10,1));
---
+++ 2-JAVASCRIPT/7-funcoes/4-funcoes-aninhadas/aula01.js
@@ -0,0 +1,7 @@
+// Funcão dentro de outra função.
+
+const soma = (...valores)=>{
+    const somar = val =>{
+
+    };    
+}
