c19d18e16d0f92ee3273c7891591b79ff5b68f82
João Vitor Da Silva Barros <jvitorbarros84@gmail.com>
2025-08-11 14:24:43 -0300

    add conteudo

--- 2-JAVASCRIPT/7-funcoes/1-declarando-funcoes/aula01.js
+++ 2-JAVASCRIPT/7-funcoes/1-declarando-funcoes/aula01.js
@@ -1,7 +1,10 @@
 // DECLARANDO FUNÇÕES
 
+console.log(soma(2, 10))
 function soma(a, b){
     return a + b;
 };
 
 console.log(soma(2, 3));
+
+//PODE CHAMARA FUNÇÃO ANTES OU DEPOIS DE DECLARA-LÁ
---
+++ 2-JAVASCRIPT/7-funcoes/2-atribuir-funcoes-a-variavel/aula01.js
@@ -0,0 +1,7 @@
+// Atribuindo uma uma função a uma variável
+
+let soma = function(a, b){
+    return a + b;
+};
+
+console.log(soma(2,10));
