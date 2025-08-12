e560a4fd5fd13e3d923da92b81a208d378c98c6b
João Vitor Da Silva Barros <jvitorbarros84@gmail.com>
2025-08-12 08:00:11 -0300

    add conteudo

--- 2-JAVASCRIPT/7-funcoes/4-funcoes-aninhadas/aula01.js
+++ 2-JAVASCRIPT/7-funcoes/4-funcoes-aninhadas/aula01.js
@@ -2,6 +2,13 @@
 
 const soma = (...valores)=>{
     const somar = val =>{
-
+        let contador = 0;
+        for(v of val){
+            contador += v;
+        }
+        return contador
     };    
+    return somar(valores);
 }
+
+console.log(soma(10,10,10,10,10));
