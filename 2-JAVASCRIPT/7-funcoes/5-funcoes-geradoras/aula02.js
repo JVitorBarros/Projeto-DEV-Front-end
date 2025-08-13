14fb9608bfc3e07daaf25efd285a30129c856eba
João Vitor Da Silva Barros <jvitorbarros84@gmail.com>
2025-08-13 07:39:12 -0300

    Adicionando conteudo na aula 7-funções

--- 2-JAVASCRIPT/7-funcoes/5-funcoes-geradoras/aula02.js
+++ 2-JAVASCRIPT/7-funcoes/5-funcoes-geradoras/aula02.js
@@ -1,16 +1,19 @@
 const entrada = require('prompt-sync')({sigint: true});
-let nContagem = Number(entrada('informe até onde ira a contagem: '));
+//let nContagem = Number(entrada('informe até onde ira a contagem: '));
 
 function* contador(){
     let i=0;
     while(true){
         yield i++
+        if(i>5){
+            break;
+        }
     }
 }
 
 const itc = contador();
-for(let i=0; i < nContagem+1 ; i++){
-    console.log(itc.next().value);
+for(c of itc){
+    console.log(c);
 }
 
 
