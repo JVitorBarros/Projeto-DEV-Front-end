c5f916ecd9820019d3aa2648134c3b6cea2ef2b5
João Vitor Da Silva Barros <jvitorbarros84@gmail.com>
2025-09-16 11:15:30 -0300

    ADD

---
+++ 3- Aplicações web/2 - DOM/4 - referenciar elementos/aula 01/aula01.js
@@ -0,0 +1,6 @@
+const caixa = document.querySelector('#primeira-caixa');
+
+console.log(caixa.getRootNode)
+//console.log(caixa.firstElementChild)
+//console.log(caixa.lastElementChild)
+//console.log(caixa.children)
